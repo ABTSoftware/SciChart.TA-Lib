c511aa7c1b9937883fcf8b30ed8ff07d24b90f66
andyb1979 <aburnettthompson@gmail.com>
2016-10-25 16:14:59 +0100

    Empty shell of TA-Lib.NET project

---
+++ SciChart.TA-Lib.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SciChart.TechnicalIndicators")]
+[assembly: AssemblyDescription("TA-Lib.NET is an open source .NET AnyCPU compatible wrapper around the popular open source TA-Lib Financial Technical Analysis Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SciChart Ltd")]
+[assembly: AssemblyProduct("TA-Lib.NET")]
+[assembly: AssemblyCopyright("Copyright © 2016 SciChart Ltd")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8b2c87d-7c9a-49ad-a48c-19a6940b0085")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
